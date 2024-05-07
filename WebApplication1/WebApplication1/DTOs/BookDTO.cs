f6d1537369b3699964b04f87f099053d867a6bd4
Błażej Kujawiński <sxxxx@pjwstk.edu.pl>
2024-05-07 11:38:28 +0200

    mass commit

---
+++ WebApplication1/WebApplication1/DTOs/BookDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.DTOs;
+
+public class BookDTO
+{
+    public int Id { get; set; }
+    public string Title { get; set; } 
+    public List<GenreDTO> GenresList { get; set; }
+}
+
+public class GenreDTO
+{
+    public string genre { get; set; }
+}
